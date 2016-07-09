bc4fa851b7bc20b2361740260d31aa17aa715c85
CrazyMatt <matthewelaw@yahoo.com>
2016-07-08 22:47:45 -0400

    Add files via upload

---
+++ mbox.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Message Box</title>
+<link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+<body>
+<div class="msgbox">
+<?php
+	echo "<p class='article'>" . $_GET["msg"] . "</p>";
+	echo "<br/>";
+	echo "<div class='b1'><a href='" . $_GET["return"] . "' class='msgboxans'><span class='grey'>OK</span></a></div>"
+?>
+</div>
+</body>
+</html>
