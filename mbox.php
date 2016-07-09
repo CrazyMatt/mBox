b3aa2f9a55a101ad37df93ac00a8c59fdbb7f6d8
CrazyMatt <matthewelaw@yahoo.com>
2016-07-08 22:55:01 -0400

    Added Indents/Doctype

--- mbox.php
+++ mbox.php
@@ -1,15 +1,16 @@
+<!DOCTYPE html>
 <html>
-<head>
-<title>Message Box</title>
-<link rel="stylesheet" type="text/css" href="main.css" />
-</head>
-<body>
-<div class="msgbox">
-<?php
-	echo "<p class='article'>" . $_GET["msg"] . "</p>";
-	echo "<br/>";
-	echo "<div class='b1'><a href='" . $_GET["return"] . "' class='msgboxans'><span class='grey'>OK</span></a></div>"
-?>
-</div>
-</body>
+	<head>
+		<title>Message Box</title>
+		<link rel="stylesheet" type="text/css" href="main.css" />
+	</head>
+	<body>
+		<div class="msgbox">
+		<?php
+			echo "<p class='article'>" . $_GET["msg"] . "</p>";
+			echo "<br/>";
+			echo "<div class='b1'><a href='" . $_GET["return"] . "' class='msgboxans'><span class='grey'>OK</span></a></div>"
+		?>
+		</div>
+	</body>
 </html>
